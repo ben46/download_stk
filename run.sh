322b580bae0ba2ba6278032da305ab903fa72b6b
Zhuoqian Zhou <ben02060846@gmail.com>
2023-11-25 10:31:50 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # cd /Users/nobbyheell/Documents/workspace/download_stk/ && python3 /Users/nobbyheell/Documents/workspace/download_stk/download_stock_list.py && python3 /Users/nobbyheell/Documents/workspace/download_stk/download_daily.py && python3 /Users/nobbyheell/Documents/workspace/download_stk/export_daily_csv.py
-cd /Users/nobbyheell/Documents/workspace/download_stk/ && python3 /Users/nobbyheell/Documents/workspace/download_stk/download_daily.py && python3 /Users/nobbyheell/Documents/workspace/download_stk/export_daily_csv.py
+cd ~/Documents/workspace/download_stk/ && python3 ~/Documents/workspace/download_stk/download_daily_by_trade_date.py && python3 ~/Documents/workspace/download_stk/export_daily_csv.py
